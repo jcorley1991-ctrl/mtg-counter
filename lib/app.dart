b23e44a1eade06007594f7d0747584be5c99be81
jcorley1991-ctrl <jcorley1991@gmail.com>
2026-08-17 12:10:16 -0600

    Add tap-to-enter 13 Spells startup screen

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class MtgCounterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MTG Counter',
+      title: '13 Spells Life Counter',
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
@@ -19,7 +19,38 @@ class MtgCounterApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const TableScreen(),
+      home: const _StartupGate(),
+    );
+  }
+}
+
+class _StartupGate extends StatefulWidget {
+  const _StartupGate();
+
+  @override
+  State<_StartupGate> createState() => _StartupGateState();
+}
+
+class _StartupGateState extends State<_StartupGate> {
+  bool _entered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_entered) return const TableScreen();
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => setState(() => _entered = true),
+        child: SizedBox.expand(
+          child: Image.asset(
+            'assets/branding/startup.jpg',
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.high,
+          ),
+        ),
+      ),
     );
   }
 }
